7dc50d35b9d56371fef90a7ed689a7ed82bb0529
Omeko-FG <gurbuzzomer@gmail.com>
2023-05-06 15:41:05 +0300

    hello world

--- Django-Install/firstproject/urls.py
+++ Django-Install/firstproject/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.http import HttpResponse
+
+
+def main(request):
+    return HttpResponse('''
+        <h1>
+        HELLO WOLRD
+        </h1>
+    ''')
 
 urlpatterns = [
+    path('', main),
     path('admin/', admin.site.urls),
 ]
